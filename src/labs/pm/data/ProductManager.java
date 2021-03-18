fe89994dac25c6dfe8a3c92e74925ef0d98f4485
Antoine <antoine.mallah@susep.gov.br>
2021-03-18 18:47:43 -0300

    Collections

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -10,8 +10,8 @@ import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -28,7 +28,7 @@ public class ProductManager {
     
     private Product product;
     
-    private Review review;
+    private Review[] reviews = new Review[5];
 
     public ProductManager(Locale locale) {
         this.locale = locale;
@@ -48,8 +48,22 @@ public class ProductManager {
     }
     
     public Product reviewProduct(Product product, Rating rating, String comments) {
-        review = new Review(rating, comments);
-        this.product = product.applyRating(rating);
+        //reviews = new Review(rating, comments);
+        if (reviews[reviews.length -1] != null) {
+            reviews = Arrays.copyOf(reviews, reviews.length + 5);
+        }
+        int sum = 0, i = 0;
+        boolean reviewed = false;
+        while (i < reviews.length && !reviewed) {
+            if (reviews[i] == null) {
+                reviews[i] = new Review(rating, comments);
+                reviewed = true;
+            }
+            sum += reviews[i].getRating().ordinal();
+            i++;
+        }
+        //this.product = product.applyRating(rating);
+        this.product = product.applyRating(Rateable.convert(Math.round((float)sum/i)));
         return this.product;
     }
 
@@ -63,16 +77,22 @@ public class ProductManager {
         
         txt.append('\n');
         
-        if (review != null) {
+        for (Review review : reviews) {
+            if (review == null) {
+                break;
+            }   
             txt.append(MessageFormat.format(resources.getString("review"), 
-                    review.getRating(),
-                    review.getComments()));
-        } else {
-            txt.append(resources.getString("no.reviews"));
+                review.getRating(),
+                review.getComments()));
+        
+            txt.append('\n');
         }
         
-        txt.append('\n');
-        
+        if (reviews[0] == null) {
+            txt.append(resources.getString("no.reviews"));
+            txt.append('\n');
+        }
+                
         System.out.println(txt);
     }
 }
--- src/testes/TestesMain.java
+++ src/testes/TestesMain.java
@@ -7,8 +7,13 @@ package testes;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 import labs.pm.data.Drink;
 import labs.pm.data.Food;
 import labs.pm.data.Product;
@@ -23,7 +28,13 @@ public class TestesMain {
     public static void main(String[] args) {
         //arrayDeclaration();
         //arraysMultidimensionais();
-        metodosArrays();
+        //metodosArrays();
+        //matrizLoopExemplo();
+        //breakContinueLabelLoop();
+        //immutableArrayList();
+        //mutableArrayList();
+        //collectionRemoveIf();
+        collectionsMethods();
     }
     
     private static void arrayDeclaration() {
@@ -93,6 +104,124 @@ public class TestesMain {
             if (o2.length() > o1.length()) return -1;
             return 0;
         };
+    }
+    
+    private static void matrizLoopExemplo() {
+        int[][] matriz = { { 1,2,3 }, { 4,5,6 }, { 7,8,9 } };
+
+        System.out.println("============================================");        
+        System.out.println("Elementos da matriz com i == j:");
+        System.out.println("============================================");
+
+        for (int i = 0, j = 0; i < matriz.length && j < matriz[0].length; i++, j++) {
+            System.out.println("matriz["+i+", "+j+"] = "+matriz[i][j]);
+        }
+        
+        System.out.println("============================================");        
+        System.out.println("Elementos da matriz com i + j == 2:");
+        System.out.println("============================================");
+
+        for (int i = 0, j = matriz[0].length - 1; i < matriz.length && j >= 0; i++, j--) {
+            System.out.println("matriz["+i+", "+j+"] = "+matriz[i][j]+" ( i + j = "+(i+j)+" )");
+        }
+
+        System.out.println("============================================");
+        System.out.println("Matriz inteira:");
+        System.out.println("============================================");
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.println("matriz["+i+", "+j+"] = "+matriz[i][j]);
+            }
+        }
+        
+        System.out.println("============================================");
+        System.out.println("Valores da matriz (foreach):");
+        System.out.println("============================================");
+
+        StringBuilder sb = new StringBuilder("Valores: ");
+        
+        for (int[] linha: matriz) {
+            for (int valor: linha) {
+                sb.append(valor).append(",");
+            }
+        }
+        System.out.println(sb.deleteCharAt(sb.lastIndexOf(",")).append('.'));
+    }
+    
+    private static void breakContinueLabelLoop() {
+        char[][] letras = {
+            {'a', 'b', 'c', 'd', 'e'}, 
+            {'f', 'g', 'h', 'i', 'j'},
+            {'k', 'l', 'm', 'n', 'o'},
+            {'p', 'q', 'r', 's', 't'},
+            {'u', 'v', 'w', 'x', 'y'},
+        };
+        umLabel: // O bloco pode ser colocado no inicio de um bloco de loop
+        for (char[] linha: letras) {
+            for (char valor: linha) {
+                switch (valor) {
+                    case 'c': continue;
+                    case 'h': continue umLabel;
+                    case 'l': break;
+                    case 'q': break umLabel;
+                    default: System.out.print(valor+",");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    
+    private static void immutableArrayList() {
+        List<String> immutable = List.of("a", "e", "i", "o", "u");
+        immutable.add("w");// ERRO! Immutable list! 
+    }
+
+    private static void mutableArrayList() {
+        List<String> mutable = Arrays.asList("a", "e", "i", "o", "u");
+        mutable.add("y"); // ERRO! Fixed-sized list!
+    }
+    
+    private static void collectionRemoveIf() {
+        Collection<Product> c = new ArrayList<>();
+        c.add(new Drink(0, "Coca", BigDecimal.valueOf(5.0), Rating.NOT_RATED));
+        c.add(new Food(1, "Hot-dog", BigDecimal.valueOf(12.5), Rating.NOT_RATED, LocalDate.now()));
+        c.add(new Drink(2, "Martini", BigDecimal.valueOf(9.9), Rating.NOT_RATED));
+        c.add(new Drink(3, "Wiskey", BigDecimal.valueOf(21.9), Rating.NOT_RATED));
+        c.add(new Drink(4, "Water", BigDecimal.valueOf(1.9), Rating.NOT_RATED));
+
+        c.removeIf((t) -> t.getPrice().compareTo(BigDecimal.TEN) < 0);
+/*      
+        c.removeIf(new Predicate<Product>() {
+            @Override
+            public boolean test(Product t) {
+               return t.getPrice().compareTo(BigDecimal.TEN) < 0;
+            }
+        });
+*/
+        System.out.println(c);
+    }
+    
+    private static void collectionsMethods() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Drink(0, "Coca", BigDecimal.valueOf(5.0), Rating.NOT_RATED));
+        products.add(new Food(1, "Hot-dog", BigDecimal.valueOf(12.5), Rating.NOT_RATED, LocalDate.now()));
+        products.add(new Drink(2, "Martini", BigDecimal.valueOf(9.9), Rating.NOT_RATED));
+        products.add(new Drink(3, "Wiskey", BigDecimal.valueOf(21.9), Rating.NOT_RATED));
+        products.add(new Drink(4, "Water", BigDecimal.valueOf(1.9), Rating.NOT_RATED));
+        
+        Collections.sort(products, (p1,p2) -> p1.getPrice().compareTo(p2.getPrice()));
+        System.out.println("Sort: " + products);
+        
+        Collections.reverse(products);
+        System.out.println("Reverse: " + products);
+       
+        Collections.shuffle(products);
+        System.out.println("Shuffle: " + products);
+        
+        Collections.fill(products, new Food(1, "Bacon", BigDecimal.ONE, Rating.NOT_RATED, LocalDate.now()));
+        System.out.println("Fill: " + products);
+        
     }
 }
 
