dbb73da513e2bc8a6a9d2f4c6643c44e979be616
Antoine <antoine.mallah@susep.gov.br>
2021-03-18 19:34:02 -0300

    Troca de array por collection e map em ProductManager

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -6,7 +6,6 @@
 package labs.pm.app;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Locale;
 import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
@@ -24,14 +23,14 @@ public class Shop {
     public static void main(String[] args) {
         ProductManager pm = new ProductManager(Locale.UK);
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-        pm.printProductReport();
+        pm.printProductReport(p1);
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea.");
         p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea.");
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea.");
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea.");
         p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "Perfect tea.");
         p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon.");
-        pm.printProductReport();
+        pm.printProductReport(p1);
 
         
 //        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
